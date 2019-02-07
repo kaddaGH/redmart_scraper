ab77d5cc9632653bb5abe694281c1f4960f0a2a8
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-07 17:20:19 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,16 +1,5 @@
-pages << {
-    page_type: 'products_search',
-    method: 'GET',
-    url: "https://api.redmart.com/v1.6.0/catalog/search?pageSize=18&sort=1024&category=energy-drinks",
-    vars: {
-        'input_type' => 'taxonomy',
-        'search_term' => '-',
-        'page' => 1
-    }
 
-
-}
-search_terms = ['Red Bull', 'RedBull', 'Energy Drink', 'Energy Drinks']
+search_terms = ['Red Bull', 'RedBull']
 search_terms.each do |search_term|
 
   pages << {
