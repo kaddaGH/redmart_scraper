f9a598163bb5a5a3327a80336568589b868398eb
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-07 11:01:55 +0100

    Update scraper.

---
+++ parsers/products_reviews.rb
@@ -0,0 +1,29 @@
+json_content = content[/(?<=\()(.+?)(?=\))/]
+
+
+data = JSON.parse(json_content)
+reviews = {}
+products = data['BatchedResults']['q0']['Results']
+products_details = page['vars']['products']
+products.each do |product|
+
+  product_id = product['ProductStatistics']['ProductId']
+  product_reviews = product['ProductStatistics']['ReviewStatistics']
+  reviews[product_id] = product_reviews
+end
+
+products_details.each do |product_details|
+
+  product_id = product_details['PRODUCT_ID'].to_s
+
+  if reviews.keys.include? product_id
+    product_details['PRODUCT_STAR_RATING'] = reviews[product_id]['AverageOverallRating'].nil? ? '' : reviews[product_id]['AverageOverallRating']
+    product_details['PRODUCT_NBR_OF_REVIEWS'] = reviews[product_id]['TotalReviewCount']
+
+  end
+
+  product_details['_collection'] = 'products'
+  outputs << product_details
+
+
+end
