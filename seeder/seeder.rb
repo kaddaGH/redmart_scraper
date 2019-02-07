9f52552eeb6b38ff30d9b22b5908898957ed24da
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-07 16:09:31 +0100

    Update seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,5 +1,3 @@
-require 'uri'
-
 pages << {
     page_type: 'products_search',
     method: 'GET',
@@ -18,7 +16,7 @@ search_terms.each do |search_term|
   pages << {
       page_type: 'products_search',
       method: 'GET',
-      url: "https://api.redmart.com/v1.6.0/catalog/search?q=#{URI.encode(search_term)}&pageSize=108&sort=1024",
+      url: "https://api.redmart.com/v1.6.0/catalog/search?q=#{search_term.gsub(/\s/,'+')}&pageSize=18&sort=1024",
       vars: {
           'input_type' => 'search',
           'search_term' => search_term,
